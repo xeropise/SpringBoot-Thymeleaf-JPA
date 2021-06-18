e723d62fded5565fd244d07d726f9463927538d6
xeropise <whrbql1@gmail.com>
2021-06-19 01:09:21 +0900

    19th June

---
+++ board/src/test/java/org/zerock/board/repository/ReplyRepositoryTests.java
@@ -0,0 +1,42 @@
+package org.zerock.board.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.board.entity.Board;
+import org.zerock.board.entity.Reply;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class ReplyRepositoryTests {
+
+    @Autowired
+    private ReplyRepository replyRepository;
+
+    @Test
+    public void insertReply() {
+
+        IntStream.rangeClosed(1, 300).forEach(i -> {
+            long bno = (long)(Math.random() * 100) + 1;
+
+            Board board = Board.builder().bno(bno).build();
+
+            Reply reply = Reply.builder().text("Reply....." +i)
+            .board(board)
+            .replyer("guest")
+            .build();
+
+            replyRepository.save(reply);
+        });
+    }
+
+    @Test
+    public void testListByBoard() {
+
+        List<Reply> replyList = replyRepository.getRepliesByBoardOrderByRno(Board.builder().bno(97L).build());
+
+        replyList.forEach(reply -> System.out.println(reply));
+    }
+}
